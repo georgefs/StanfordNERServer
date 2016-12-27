ce961bb942dabee1445a081157e28595844d92c3
Ping Chu Hung <banyhong@gmail.com>
2016-12-27 14:56:16 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -10,9 +10,9 @@ rm -f stanford-ner-2015-12-09.zip
 cp -r stanford-ner-2015-12-09/* .
 rm -rf stanford-ner-2015-12-09
 
-unzip -o http://nlp.stanford.edu/software/stanford-german-2015-10-14-models.jar
-unzip -o http://nlp.stanford.edu/software/stanford-spanish-corenlp-2015-10-14-models.jar
-unzip -o http://nlp.stanford.edu/software/stanford-chinese-corenlp-2015-12-08-models.jar
+unzip -o stanford-german-2015-10-14-models.jar
+unzip -o stanford-spanish-corenlp-2015-10-14-models.jar
+unzip -o stanford-chinese-corenlp-2015-12-08-models.jar
 unzip -o stanford-ner-3.6.0.jar
 cp lib/slf4j-api.jar .
 cp lib/slf4j-simple.jar .
